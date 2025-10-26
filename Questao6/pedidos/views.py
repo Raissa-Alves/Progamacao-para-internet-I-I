2f573eb49bff52de4d9833475d11ebaf4a14e4d9
Raíssa <raissaalvees99@gmail.com>
2025-10-26 10:56:13 -0300

    Alterando parâmetro da view

--- Questao6/pedidos/views.py
+++ Questao6/pedidos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 
-def pedido (request,nome,produto):
+def pedido (request,produto):
     return HttpResponse(f'Seus pedidos: {produto}')
